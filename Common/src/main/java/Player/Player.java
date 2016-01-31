77c9baf0a5159c2145252f3adae603f9266e1182
Jonathan Gingerich <Bozerg@users.noreply.github.com>
2016-01-31 17:50:57 -0600

    Creating Common Project for objects/logic that are common to both server and client.

---
+++ Common/src/main/java/Player/Player.java
@@ -0,0 +1,7 @@
+package Player;
+
+/**
+ * Created by Bozerg on 1/31/2016.
+ */
+public class Player {
+}
---
+++ Common/src/main/java/World/Chunk.java
@@ -0,0 +1,7 @@
+package World;
+
+/**
+ * Created by Bozerg on 1/31/2016.
+ */
+public class Chunk {
+}
